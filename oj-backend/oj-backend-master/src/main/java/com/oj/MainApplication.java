d457230a25ff764a268827751ba6f7d540a9f93b
Katrina55553 <ambition55553@gmail.com>
2026-06-07 20:40:51 +0800

    fix: 排除 Redis/Session 自动配置（JWT 不需要）

--- oj-backend/oj-backend-master/src/main/java/com/oj/MainApplication.java
+++ oj-backend/oj-backend-master/src/main/java/com/oj/MainApplication.java
@@ -3,12 +3,17 @@ package com.oj;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.data.redis.RedisRepositoriesAutoConfiguration;
+import org.springframework.boot.autoconfigure.session.SessionAutoConfiguration;
 import org.springframework.context.annotation.EnableAspectJAutoProxy;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
-
-// todo 如需开启 Redis，须移除 exclude 中的内容
-@SpringBootApplication
+@SpringBootApplication(exclude = {
+        RedisAutoConfiguration.class,
+        RedisRepositoriesAutoConfiguration.class,
+        SessionAutoConfiguration.class
+})
 @MapperScan("com.oj.mapper")
 @EnableScheduling
 @EnableAspectJAutoProxy(proxyTargetClass = true, exposeProxy = true)
