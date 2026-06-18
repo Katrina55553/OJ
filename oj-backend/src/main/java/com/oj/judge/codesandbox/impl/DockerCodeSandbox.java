785582e9d21a7d66c909d66b77eb6dd019fec224
Katrina55553 <ambition55553@gmail.com>
2026-06-18 19:02:05 +0800

    Revert "fix: PreDestroy 改用 javax.annotation，与项目其它文件保持一致"
    This reverts commit 64c72bf7e12f7db8dc1a1a8298595720136d4a8f.

--- oj-backend/src/main/java/com/oj/judge/codesandbox/impl/DockerCodeSandbox.java
+++ oj-backend/src/main/java/com/oj/judge/codesandbox/impl/DockerCodeSandbox.java
@@ -10,7 +10,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
-import javax.annotation.PreDestroy;
+import jakarta.annotation.PreDestroy;
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.*;
